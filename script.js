fbc1161df9ee4287604862cf74cdd9fba17bf2b1
abrahamalanya <alanyaoficial@gmail.com>
2023-07-19 21:24:36 +0200

    feat(linktree): Add javascript profile--links

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// profile--link
+let links = [
+  {
+    name: "github",
+    title: "Repositorio",
+    url: "https://github.com/abrahamalanya",
+    icon: "https://cdn.simpleicons.org/github/black"
+  },
+  {
+    name: "devto",
+    title: "Blog Dev.to",
+    url: "https://dev.to/pacmankabh",
+    icon: "https://cdn.simpleicons.org/devdotto/black"
+  },
+  {
+    name: "gitlab",
+    title: "Repositorio",
+    url: "https://gitlab.com/abrahamalanya",
+    icon: "https://cdn.simpleicons.org/gitlab/black"
+
+  }
+]
+
+let profileLink = document.querySelector(".profile--link");
+
+for (let link of links) {
+  let html = `
+    <div class="card">
+      <a href="${link.url}" target="_blank">
+        <div>
+          <img src="${link.icon}" alt="${link.name}">
+        </div>
+        <p>${link.title}</p>
+      </a>
+    </div>
+  `;
+
+  profileLink.innerHTML += html;
+}
+
+// profile--social
 let socialMediaLinks = [
   {
     name: "linkedin",
